a6a631c05f920f5fa951dc27df2c763f8e71fe7e
kirkV <kvande85@uwo.ca>
2023-03-30 18:44:51 -0400

    pushing smaller video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,10 @@
-/*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+$('.flipBtn').on('mousedown', function(){
+	console.log("flippin");
+	$(this).parents('.flipCard').toggleClass('flipped').children('.face').toggleClass('frontVisible');
+});
 
-(function($) {
-
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
-
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
-
-})(jQuery);
+$('#sendEmailBtn').on('click', function(){
+	var subject = $('#subject').val();
+	var msg = `${$('#message').val()} - \n ${$('#name').val()}`;
+	window.open(`mailto:kvande85@uwo.ca?subject=${subject}&body=${msg}`);
+})
